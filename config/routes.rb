a6c0a22dfdae2eb321765fdb5ce02d0dd25a9e5f
yuccaaaa <yucayann5@yahoo.co.jp>
2017-10-01 21:04:51 +0900

    made tweet_app

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get "/posts/index" => "posts#index"
+  get "/posts/:id" => "posts#show"
+
+  get "/" => "home#top"
+  get "/about" => "home#about"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
